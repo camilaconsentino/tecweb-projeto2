c9e780c2905c9b78639659a2b475edeeaa92d675
camilaconsentino <camilacc5Gal.insper.edu.br>
2023-10-24 09:26:22 -0300

    projeto criado

---
+++ agenda/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
